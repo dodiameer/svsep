d3efc3228ccafa12874fdaed0fc70803df570c35
dodiameer <mtxshiftg@gmail.com>
2021-06-18 17:04:02 +0300

    mod: customfiles

---
+++ src/customfiles/mod.rs
@@ -0,0 +1,65 @@
+use crate::fileops::FileOperations;
+use std::error::Error;
+use std::path::PathBuf;
+
+pub trait CustomFile {
+  fn new(file_content: &String, file_path: &PathBuf) -> Self;
+  fn write(&self) -> Result<(), Box<dyn Error>>;
+}
+
+pub struct ScriptFile {
+  content: String,
+  svelte_file: PathBuf,
+}
+
+impl CustomFile for ScriptFile {
+   fn new(svelte_file_content: &String, svelte_file_path: &PathBuf) -> ScriptFile {
+    ScriptFile {
+      content: svelte_file_content.to_string(),
+      svelte_file: svelte_file_path.clone(),
+    }
+  }
+   fn write(
+    &self,
+  ) -> std::result::Result<(), std::boxed::Box<(dyn std::error::Error + 'static)>> {
+    FileOperations::write_file(&self.svelte_file, ".js", &self.content)
+  }
+}
+
+pub struct StyleFile {
+  content: String,
+  svelte_file: PathBuf,
+}
+
+impl CustomFile for StyleFile {
+   fn new(svelte_file_content: &String, svelte_file_path: &PathBuf) -> StyleFile {
+    StyleFile {
+      content: svelte_file_content.to_string(),
+      svelte_file: svelte_file_path.clone(),
+    }
+  }
+   fn write(
+    &self,
+  ) -> std::result::Result<(), std::boxed::Box<(dyn std::error::Error + 'static)>> {
+    FileOperations::write_file(&self.svelte_file, ".css", &self.content)
+  }
+}
+
+pub struct MarkupFile {
+  content: String,
+  svelte_file: PathBuf,
+}
+
+impl CustomFile for MarkupFile {
+   fn new(svelte_file_content: &String, svelte_file_path: &PathBuf) -> MarkupFile {
+    MarkupFile {
+      content: svelte_file_content.to_string(),
+      svelte_file: svelte_file_path.clone(),
+    }
+  }
+   fn write(
+    &self,
+  ) -> std::result::Result<(), std::boxed::Box<(dyn std::error::Error + 'static)>> {
+    FileOperations::write_file(&self.svelte_file, ".html", &self.content)
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,15 @@
 mod fileops;
+mod customfiles;
 use std::error::Error;
 use std::path::PathBuf;
 use structopt::StructOpt;
-use fileops::FileOperations;
+use crate::fileops::FileOperations;
+use crate::customfiles::{ScriptFile, MarkupFile, StyleFile, CustomFile}; // Custom file trait required - don't remove
 
 #[derive(StructOpt, Debug)]
 struct InputArgs {
     file: PathBuf,
 }
-
-
-trait CustomFile {
-    fn new(file_content: &String, file_path: &PathBuf) -> Self;
-    fn write(&self) -> Result<(), Box<dyn Error>>;
-}
-
-struct ScriptFile {
-    content: String,
-    svelte_file: PathBuf,
-}
-
-impl CustomFile for ScriptFile {
-    fn new(svelte_file_content: &String, svelte_file_path: &PathBuf) -> ScriptFile {
-        ScriptFile {
-            content: svelte_file_content.to_string(),
-            svelte_file: svelte_file_path.clone(),
-        }
-    }
-    fn write(&self) -> std::result::Result<(), std::boxed::Box<(dyn std::error::Error + 'static)>> {
-        FileOperations::write_file(&self.svelte_file, ".js", &self.content)
-    }
-}
-
-struct StyleFile {
-    content: String,
-    svelte_file: PathBuf,
-}
-
-impl CustomFile for StyleFile {
-    fn new(svelte_file_content: &String, svelte_file_path: &PathBuf) -> StyleFile {
-        StyleFile {
-            content: svelte_file_content.to_string(),
-            svelte_file: svelte_file_path.clone(),
-        }
-    }
-    fn write(&self) -> std::result::Result<(), std::boxed::Box<(dyn std::error::Error + 'static)>> {
-        FileOperations::write_file(&self.svelte_file, ".css", &self.content)
-    }
-}
-
-struct MarkupFile {
-    content: String,
-    svelte_file: PathBuf,
-}
-
-impl CustomFile for MarkupFile {
-    fn new(svelte_file_content: &String, svelte_file_path: &PathBuf) -> MarkupFile {
-        MarkupFile {
-            content: svelte_file_content.to_string(),
-            svelte_file: svelte_file_path.clone(),
-        }
-    }
-    fn write(&self) -> std::result::Result<(), std::boxed::Box<(dyn std::error::Error + 'static)>> {
-        FileOperations::write_file(&self.svelte_file, ".html", &self.content)
-    }
-}
-
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = InputArgs::from_args();
