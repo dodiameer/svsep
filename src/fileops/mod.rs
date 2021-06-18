c387207a88417f051a31e86a758f9746f885545f
dodiameer <mtxshiftg@gmail.com>
2021-06-18 18:08:39 +0300

    refactor: path_to_string

--- src/fileops/mod.rs
+++ src/fileops/mod.rs
@@ -6,13 +6,13 @@ use std::path::{Path, PathBuf};
 
 pub struct FileOperations;
 
-pub fn path_to_string(path: &Path) -> String {
-  String::from(path.display().to_string())
-}
-
 impl FileOperations {
+  pub fn path_to_string(path: &Path) -> String {
+    String::from(path.display().to_string())
+  }
+
   pub fn write_file(path: &PathBuf, extention: &str, content: &String) -> Result<()> {
-    let file_name = path_to_string(&path).replace(".svelte", extention);
+    let file_name = Self::path_to_string(&path).replace(".svelte", extention);
     let file_name = file_name.as_str();
     let mut file =
       File::create(file_name).with_context(|| format!("Unable to create file `{}`", file_name))?;
@@ -23,7 +23,7 @@ impl FileOperations {
     Ok(())
   }
   pub fn read_to_string(path: &PathBuf) -> Result<String> {
-    let file = File::open(path).with_context(|| format!("Unable to read file `{}`", path_to_string(&path)))?;
+    let file = File::open(path).with_context(|| format!("Unable to read file `{}`", Self::path_to_string(&path)))?;
     let reader = BufReader::new(file);
     let mut content = String::new();
     for line in reader.lines().map(|l| l.unwrap()) {
